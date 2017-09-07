72d0db76d046545a0cf9a47b3025bb54136817fc
Andy Boyle <andy@sudostream.io>
2017-09-07 14:09:01 +0100

    0.0.18 Convert insert to many and also use futures

---
+++ src/main/scala/io/sudostream/timetoteach/esandospopulator/EsAndOsInserter.scala
@@ -0,0 +1,85 @@
+package io.sudostream.timetoteach.esandospopulator
+
+import argonaut.Argonaut._
+import argonaut._
+import org.mongodb.scala.MongoCollection
+import org.mongodb.scala.{Completed, Document, Observer, SingleObservable}
+
+import scala.concurrent.Future
+import scala.io.Source
+
+trait EsAndOsInserter {
+
+  def decodeEsAndOsForDatabaseInjestion: List[EAndO] = {
+    val filename = "/esAndOs.json"
+    val filenameAsInputStream = getClass.getResourceAsStream(filename)
+    val input = Source.fromInputStream(filenameAsInputStream).mkString
+    input.decodeOption[List[EAndO]].getOrElse(Nil)
+  }
+
+  def insertEsAndOsToDatabase(esAndOs: List[EAndO],
+                              esAndOsCollection: MongoCollection[Document]): Future[Completed] = {
+    val docsToInsert: Seq[Document] = esAndOs.map {
+      eo =>
+        val esAndOsAsDocuments: List[Document] = eo.experienceAndOutcome.map {
+          sentence =>
+            Document("sentence" -> sentence.sentence,
+              "bulletPoints" -> sentence.bulletPoints)
+        }
+
+        Document(
+          "experienceAndOutcome" -> esAndOsAsDocuments,
+          "codes" -> eo.codes,
+          "curriculumLevels" -> eo.curriculumLevels,
+          "curriculumAreaName" -> eo.curriculumAreaName,
+          "eAndOSetName" -> eo.eAndOSetName,
+          "eAndOSetSectionName" -> eo.eAndOSetSectionName,
+          "eAndOSetSubSectionName" -> eo.eAndOSetSubSectionName,
+          "eAndOSetSubSectionAuxiliaryText" -> eo.eAndOSetSubSectionAuxiliaryText,
+          "responsibilityOfAllPractitioners" -> eo.responsibilityOfAllPractitioners
+        )
+    }
+    esAndOsCollection.insertMany(docsToInsert).toFuture
+  }
+
+
+}
+
+case class EAndO(experienceAndOutcome: List[Sentence],
+                 codes: List[String],
+                 curriculumLevels: List[String],
+                 curriculumAreaName: String,
+                 eAndOSetName: Option[String],
+                 eAndOSetSectionName: Option[String],
+                 eAndOSetSubSectionName: Option[String],
+                 eAndOSetSubSectionAuxiliaryText: Option[String],
+                 responsibilityOfAllPractitioners: Boolean
+                )
+
+object EAndO {
+  implicit def EAndOCodecJson: CodecJson[EAndO] =
+    casecodec9(EAndO.apply, EAndO.unapply)(
+      "experienceAndOutcome",
+      "codes",
+      "curriculumLevels",
+      "curriculumAreaName",
+      "eAndOSetName",
+      "eAndOSetSectionName",
+      "eAndOSetSubSectionName",
+      "eAndOSetSubSectionAuxiliaryText",
+      "responsibilityOfAllPractitioners"
+    )
+}
+
+case class Sentence(
+                     sentence: String,
+                     bulletPoints: List[String]
+                   )
+
+object Sentence {
+  implicit def SentenceCodeJson: CodecJson[Sentence] =
+    casecodec2(Sentence.apply, Sentence.unapply)(
+      "sentence",
+      "bulletPoints"
+    )
+}
--- src/main/scala/io/sudostream/timetoteach/esandospopulator/EsAndOsPopulator.scala
+++ src/main/scala/io/sudostream/timetoteach/esandospopulator/EsAndOsPopulator.scala
@@ -1,67 +1,21 @@
 package io.sudostream.timetoteach.esandospopulator
 
-import argonaut.Argonaut._
-import argonaut._
-//import io.sudostream.timetoteach.esandospopulator.EsAndOsPopulator.esAndOsCollection
 import org.mongodb.scala._
 
-object EsAndOsPopulator extends App with MongoDbHelper with ConsoleMessages {
+import scala.concurrent.Future
+
+object EsAndOsPopulator extends App with MongoDbHelper with EsAndOsInserter with ConsoleMessages {
   println(startupMessage)
 
-  import scala.io.Source
+  val esAndOs: List[EAndO] = decodeEsAndOsForDatabaseInjestion
+  val esAndOsCollection: MongoCollection[Document] = getEsAndOsCollection
+  val insertToDbFuture: Future[Completed] = insertEsAndOsToDatabase(esAndOs, esAndOsCollection)
 
-  val filename = "/esAndOs.json"
-  val filenameAsInputStream = getClass.getResourceAsStream(filename)
-  val input = Source.fromInputStream(filenameAsInputStream).mkString
-
-
-  // parse the string as json, attempt to decode it to a list of person,
-  // otherwise just take it as an empty list.
-  val esAndOs = input.decodeOption[List[EAndO]].getOrElse(Nil)
-
-//  esAndOs.foreach(eAndO => println("EAndO\n-------\n" + eAndO + "\n"))
-
-  // convert back to json, and then to a pretty printed string, alternative
-  // ways to print may be nospaces, spaces2, or a custom format
-
-
-  ///
-  val esAndOsCollection = getEsAndOsCollection
-
-  esAndOs.foreach {
-    eo => {
-
-      val esAndOsAsDocuments: List[Document] = eo.experienceAndOutcome.map {
-        sentence =>
-          Document("sentence" -> sentence.sentence,
-            "bulletPoints" -> sentence.bulletPoints)
-      }
-
-      val eoDoc = Document(
-        "experienceAndOutcome" -> esAndOsAsDocuments,
-        "codes" -> eo.codes,
-        "curriculumLevels" -> eo.curriculumLevels,
-        "curriculumAreaName" -> eo.curriculumAreaName,
-        "eAndOSetName" -> eo.eAndOSetName,
-        "eAndOSetSectionName" -> eo.eAndOSetSectionName,
-        "eAndOSetSubSectionName" -> eo.eAndOSetSubSectionName,
-        "eAndOSetSubSectionAuxiliaryText" -> eo.eAndOSetSubSectionAuxiliaryText,
-        "responsibilityOfAllPractitioners" -> eo.responsibilityOfAllPractitioners
-      )
-
-      val insertObservable = esAndOsCollection.insertOne(eoDoc)
-
-      insertObservable.subscribe(
-        new Observer[Completed] {
-          override def onNext(result: Completed): Unit = println(s"onNext: $result")
-          override def onError(e: Throwable): Unit = println(s"onError: $e")
-          override def onComplete(): Unit = println("onComplete")
-        }
-      )
-    }
+  while (!insertToDbFuture.isCompleted) {
+    println(s"Waiting for db inserts of E's & O's to complete...")
+    Thread.currentThread().join(100L)
   }
 
-  Thread.sleep(180L)
   println(finishedMessage)
   System.exit(0)
 }
@@ -71,41 +25,3 @@ trait ConsoleMessages {
   lazy val finishedMessage = "EsAndOsPopulator done."
 }
 
-case class EAndO(experienceAndOutcome: List[Sentence],
-                 codes: List[String],
-                 curriculumLevels: List[String],
-                 curriculumAreaName: String,
-                 eAndOSetName: Option[String],
-                 eAndOSetSectionName: Option[String],
-                 eAndOSetSubSectionName: Option[String],
-                 eAndOSetSubSectionAuxiliaryText: Option[String],
-                 responsibilityOfAllPractitioners: Boolean
-                )
-
-object EAndO {
-  implicit def EAndOCodecJson: CodecJson[EAndO] =
-    casecodec9(EAndO.apply, EAndO.unapply)(
-      "experienceAndOutcome",
-      "codes",
-      "curriculumLevels",
-      "curriculumAreaName",
-      "eAndOSetName",
-      "eAndOSetSectionName",
-      "eAndOSetSubSectionName",
-      "eAndOSetSubSectionAuxiliaryText",
-      "responsibilityOfAllPractitioners"
-    )
-}
-
-case class Sentence(
-                     sentence: String,
-                     bulletPoints: List[String]
-                   )
-
-object Sentence {
-  implicit def SentenceCodeJson: CodecJson[Sentence] =
-    casecodec2(Sentence.apply, Sentence.unapply)(
-      "sentence",
-      "bulletPoints"
-    )
-}
